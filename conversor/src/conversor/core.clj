613a435b64daefa05a300e9349d4e7ecf61f92ea
Luiz Menezes <lcmscontas@gmail.com>
2021-02-17 21:32:54 -0300

    Finalizing API

--- conversor/src/conversor/core.clj
+++ conversor/src/conversor/core.clj
@@ -1,28 +1,11 @@
 (ns conversor.core
-  (:require [clojure.tools.cli :refer [parse-opts]]
-            [cheshire.core :refer :all]
-            [clj-http.client :as http-client])
-  (:gen-class))
-
-(def chave (System/getenv "CHAVE_API"))
-
-(def api-url
-  "https://free.currencyconverterapi.com/api/v6/convert")
-
-(def usd-brl
-  (:body (http-client/get api-url
-          {:query-params {"q" "USD_BRL"
-                          "apiKey" chave}})))
-
-(parse-string usd-brl)
-
-(defn parametrizar-moedas [de para]
-  (str de "_" para))
-
+  (:require [conversor.formatador :refer [formatar]]
+            [conversor.interpretador-de-opcoes :refer
+             [interpretar-opcoes]]
+            [conversor.cambista :refer [obter-cotacao]]))
 (defn -main
   [& args]
-  (let [{:keys [de para]} (:options
-                           (parse-opts args opcoes-do-programa))]
-        (prn "Cotação:" (http-client/get api-url
-            {:query-params {"q" (parametrizar-moedas de para)
-                            "apiKey" chave}}))))
+  (let [{:keys [de para]} (interpretar-opcoes args)]
+    (-> (obter-cotacao de para)
+        (formatar de para)
+        (prn))))
