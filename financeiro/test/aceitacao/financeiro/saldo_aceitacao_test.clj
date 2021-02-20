8fb9a3dcc0ec1fab77316219514f6f293e861b78
Luiz Menezes <lcmscontas@gmail.com>
2021-02-20 20:05:16 -0300

    Evolução API - Saldo inicial

---
+++ financeiro/test/aceitacao/financeiro/saldo_aceitacao_test.clj
@@ -0,0 +1,9 @@
+(ns financeiro.saldo-aceitacao-test
+  (:require [midje.sweet :refer :all]
+            [cheshire.core :as json]
+            [financeiro.auxiliares :refer :all]))
+
+(against-background [(before :facts (iniciar-servidor porta-padrao))
+                     (after :facts (parar-servidor))]
+                    (fact "O saldo inicial é 0" :aceitacao
+                          (json/parse-string (conteudo "/saldo")) => {:saldo 0}))
