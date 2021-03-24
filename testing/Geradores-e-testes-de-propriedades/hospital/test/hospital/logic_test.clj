30a0b702679dffb0fc385c0f7b060c3b2da7eb30
Luiz Carlos Menezes Santos <luiz.santos@paygo.com.br>
2021-03-24 09:19:36 -0300

    testdando Defspec e prop/for-all

--- testing/Geradores-e-testes-de-propriedades/hospital/test/hospital/logic_test.clj
+++ testing/Geradores-e-testes-de-propriedades/hospital/test/hospital/logic_test.clj
@@ -3,6 +3,8 @@
             [hospital.logic :as lg]
             [hospital.model :as h.model]
             [clojure.test.check.generators :as gen]
+            [clojure.test.check.clojure-test :refer (defspec)]
+            [clojure.test.check.properties :as prop]
             [schema.core :as s]))
 
 (s/set-fn-validation! true)
@@ -12,6 +14,10 @@
 
   (testing "Que cabe numa fila vazia"
     (is (lg/cabe-na-fila? {:espera []}, :espera)))
+
+  (testing "Que cabe pessoas em filas de tamanho até 4 inclusive"
+    (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 100)]
+      (is (lg/cabe-na-fila? {:espera fila} :espera))))
 
   (testing "Que não cabe na fila quando a fila está cheia"
 
@@ -27,3 +33,19 @@
   (testing "Que não cabe quando o departamento não existe"
     (is (not (lg/cabe-na-fila? {:espera [1 2 3 4]}, :raio-x)))))
 
+;; (deftest chega-em-test
+;;   (testing "Que é colocada uma pessoa em filas menores que 5"
+;;     (doseq [fila (gen/sample (gen/vector gen/string-alphanumeric 0 4) 10)
+;;             pessoa (gen/sample gen/string-alphanumeric 5 )]
+;;       (println pessoa fila)
+;;       ;; só para mostrar que são 50 asserts (10 * 5)
+;;       )
+;;     ))
+
+(defspec explorando-a-api 10
+  (prop/for-all
+   [fila (gen/vector gen/string-alphanumeric 0 4)
+    pessoa gen/string-alphanumeric]
+   (println pessoa fila)
+   true
+   ))
