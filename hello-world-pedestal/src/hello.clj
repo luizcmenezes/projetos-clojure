06c5c2706eab8f2f96084623edd61f10fcbdae02
Luiz Carlos Menezes Santos <luiz.santos@paygo.com.br>
2021-05-03 16:32:47 -0300

    Melhorando a API com a passagem de query-params

--- hello-world-pedestal/src/hello.clj
+++ hello-world-pedestal/src/hello.clj
@@ -1,19 +1,52 @@
 (ns hello
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [clojure.string :as str]))
+
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(defn greetting-for [nm]
+  (cond
+    (unmentionables nm) nil
+    (empty? nm) "Hello World\n"
+    :else (str "Hello, " nm "\n")))
 
 (defn respond-hello [request]
-  {:status 200 :body "Hello, world!"})
+  (let [nm (get-in request [:query-params :name])
+        resp (greetting-for (str/capitalize nm))]
+    (if resp
+      (ok resp)
+      (not-found))))
 
 (def routes
   (route/expand-routes
    #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
-   {::http/routes routes
-    ::http/type :jetty
-    ::http/port 8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
